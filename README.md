7878963636636026a40dd16599f44333f25444d4
MatheusMenecucci23 <menecucci40@gmail.com>
2021-03-16 18:38:37 -0300

    :package: menu sidebar

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 # Portf-lio2021
+
 Portfólio do desenvolvedor Matheus Menecucci
 
 ## Terefas
+
 O controle das tarefas desse projeto será realizado no GitHub
+
+## Ícones
+
+- :package: nova funcionalidade
+- :up:atualização
+- :beetle:correção de bugs
+- :checkered_flag:release
---
+++ script.js
@@ -0,0 +1,6 @@
+var menuHamburguer = document.querySelector(".menu-hamburguer");/*selecionando o elemento*/
+
+menuHamburguer.addEventListener("click",function(){/*add um evento e criando a funcionalidade*/ 
+    document.querySelector(".container").classList.toggle("show-menu")/*.toggle remover ou adicionar uma classe*/
+    /*pegando todos os elementos com container*/  
+});
